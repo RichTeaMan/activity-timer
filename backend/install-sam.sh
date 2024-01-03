1c7c25f8166afbf36bdabf98b3865fda41619488
Thomas Holmes <thomasholmes@outlook.com>
2024-01-03 13:08:14 +0000

    Added install SAM script.

---
+++ backend/install-sam.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+curl -L https://github.com/aws/aws-sam-cli/releases/latest/download/aws-sam-cli-linux-x86_64.zip -o aws-sam-cli-linux-x86_64.zip
+unzip aws-sam-cli-linux-x86_64.zip -d sam-installation
+sudo ./sam-installation/install
+rm ./sam-installation/ -r
+rm aws-sam-cli-linux-x86_64.zip
+sam --version
